92768cc76586cd2fb437faa580dc0105b77c7b37
shaurya35 <shauryajha35@gmail.com>
2024-05-08 19:12:37 +0530

    add boilerplate to index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1> hello world </h1>');
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
