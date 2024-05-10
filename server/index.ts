144150a37bc2b4274d58c1cdd52e78026c898564
shaurya35 <shauryajha35@gmail.com>
2024-05-10 18:16:13 +0530

    add database

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+const mongoose = require("mongoose");
 
 //dotenv
 require("dotenv").config();
@@ -23,6 +24,17 @@ app.get("/", (req, res) => {
 app.use("/admin", adminRoute);
 app.use("/quiz", quizRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port", process.env.PORT);
-});
+//connect db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port", process.env.PORT);
+    });
+
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
