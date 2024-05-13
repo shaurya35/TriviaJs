7cb51119010d964c3fdb511ee9aa5dc87a0b511c
shaurya35 <shauryajha35@gmail.com>
2024-05-13 23:17:58 +0530

    clean admin panel

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -3,49 +3,49 @@ const router = express.Router();
 
 //admmin panel route
 router.get('/', (req, res) => {
-    res.send('this is admin route');
+    res.send('admin here');
 });
 
 
 //admin panel see all profiles
 router.get('/all-profiles', (req, res) => {
-    res.send('this is admin 101 route');
+    res.send('admin here- all profiles');
 });
 
 
 //admin profile see all questions
 router.get('/questions', (req, res) => {
-    res.send('this is admin 102 route');
+    res.send('admin here- questions ');
 });
 
 
 //admin panel easy
 router.get('/questions/easy', (req, res) => {
-    res.send('this is admin 102 route');
+    res.send('admin here- questions - getEasy');
 });
 
 router.post('/questions/easy', (req, res) => {
-    res.send('this is admin 102 route');
+    res.send('admin here- questions - postEasy');
 });
 
 
 //admin panel med
 router.get('/questions/medium', (req, res) => {
-    res.send('this is admin 102 route');
+    res.send('admin here- questions - getMed');
 });
 
 router.post('/questions/medium', (req, res) => {
-    res.send('this is admin 102 route');
+    res.send('admin here- questions - postMed');
 });
 
 
 //admin panel hard
 router.get('/questions/hard', (req, res) => {
-    res.send('this is admin 102 route');
+    res.send('admin here- questions - getHard');
 });
 
 router.post('/questions/hard', (req, res) => {
-    res.send('this is admin 102 route');
+    res.send('admin here- questions - postHard');
 });
 
 // export the router module so that indexcan .js file can use it
