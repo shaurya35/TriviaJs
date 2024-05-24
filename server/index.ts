36a8524e8a8a971662ae9ae458084fba1be220e8
shaurya35 <shauryajha35@gmail.com>
2024-05-24 22:31:44 +0530

    add paranthesis

--- server/index.ts
+++ server/index.ts
@@ -45,7 +45,7 @@ app.use("/quiz", quizRoute);
 app.use(function (err: any, req: any, res: any, next: any) {
   console.error(err.stack);
   res.status(500).send("Something broke!");
-}
+});
 
 // connect db
 mongoose
