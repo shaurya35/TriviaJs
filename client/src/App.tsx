ba54eddfe39fbbd5da4494488305e3d3bc041c4f
shaurya35 <shauryajha35@gmail.com>
2024-05-26 22:56:11 +0530

    figure out CORS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,10 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [backend, setBackendData] = useState<string>('');
-
-  useEffect(() => {
-    fetch('http://localhost:8080/')
-      .then(response => response.json())
-      .then(data => {
-        setBackendData(JSON.stringify(data)); 
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
 
   return (
     <>
-      <div className="App">
-        <p>{backend}</p>
-      </div>
+
     </>
   );
 }
